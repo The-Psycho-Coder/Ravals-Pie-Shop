4ee4938652e9ac29eab77ee15bb1b57098a0e998
Kush Raval <kush.raval.01@gmail.com>
2025-05-07 16:55:59 +0530

    Ravals Pie Shop Phase Two

--- RavalsPieShop/Controllers/PieController.cs
+++ RavalsPieShop/Controllers/PieController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using RavalsPieShop.Models.Interfaces;
+using RavalsPieShop.ViewModels;
 
 namespace RavalsPieShop.Controllers
 {
-    public class PieController : ControllerBase
+    public class PieController : Controller
     {
         private readonly IPieRepository _pieRepository;
         private readonly ICategoryRepository _categoryRepository;
@@ -15,6 +17,11 @@ namespace RavalsPieShop.Controllers
             _categoryRepository = categoryRepository;
         }
 
+        public IActionResult List()
+        {
+            PieListViewModel piesListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese cakes");
+            return View(piesListViewModel);
+        }
 
     }
 }
